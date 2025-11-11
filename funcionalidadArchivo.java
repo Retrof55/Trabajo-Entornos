026752834a89609318f88798b0cafd98caf0971f
balatrower <power333xd@gmail.com>
2025-11-11 13:57:10 +0100

    Archivo funcionalidadArchivo.java editado con el conflicto resuelto

--- funcionalidadArchivo.java
+++ funcionalidadArchivo.java
@@ -1,3 +1,1 @@
-VOID main() { 
-	IO.println(Archivo para generar conflictos, trabajaremos en esta rama, Edson y Marcos intentando crear conflictos) 
-}
+void main() { IO.println(Archivo para generar conflictos, trabajaremos en esta rama, Edson y Marcos intentando crear conflictos) }
